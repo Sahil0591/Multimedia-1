c58e835afeed83155e78ef15ca045d627d26aeee
Sahil_S <113634764+Sahil0591@users.noreply.github.com>
2023-11-16 14:32:41 +0000

    Add files via upload

---
+++ js/myScript.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    setInterval(function () {
+
+        var seconds = new Date().getSeconds();
+        var sdegree = seconds * 6;
+        var srotate = "rotate(" + sdegree + "deg)";
+
+        var minutes = new Date().getMinutes();
+        var mdegree = minutes * 6;
+        var mrotate = "rotate(" + mdegree + "deg)";
+
+        var hours = new Date().getMinutes();
+        var hdegree = hours * 6;
+        var hrotate = "rotate(" + hdegree + "deg)";
+
+        $("#sec").css({"transform": srotate});
+        $("#min").css({"transform": mrotate});
+        $("#hour").css({"transform": hrotate});
+    }, 1000);
+});
